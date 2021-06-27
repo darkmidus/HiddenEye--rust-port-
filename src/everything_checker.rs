15e37ff8f8624518329db4b1ee39a161128f73d6
Darkmidus <zachary.hand.is.awesome@gmail.com>
2021-06-27 13:03:54 -0500

    Eula Checker is finally truly finished.

--- src/everything_checker.rs
+++ src/everything_checker.rs
@@ -1,5 +1,6 @@
 #[path = "extra.rs"] mod extra;
 use std::fs::File;
+use std::io::Write;
 use std::process::exit;
 use serde_json::de::from_reader;
 use colored::Colorize;
@@ -8,7 +9,7 @@ pub fn eula_checker() {
     // Opens the Eula and gets the eula value
     let file = File::open("eula.json")
         .expect("The file should open in read only");
-    let json: serde_json::Value = from_reader(file)
+    let mut json: serde_json::Value = from_reader(file)
         .expect("File should be proper json");
     let eula = json.get("eula")
         .expect("Eula was not found");
@@ -18,7 +19,12 @@ pub fn eula_checker() {
         print!("{}", "Hiddeneye can only be used for educational purposes, do you agree to these terms? (yes/no): ".blue());
         let answer = extra::hidden_input();
         if answer == "yes" {
-            println!("You accepted the terms and conditions.")
+            println!("You accepted the terms and conditions.");
+            *json.pointer_mut("/eula").unwrap() = true.into();
+            let json_string = json.to_string();
+            let mut f = File::create("eula.json").expect("Unable to create file");
+            f.write_all(json_string.as_bytes()).expect("Unable to write data");
+
         }
         else if answer == "no" {
             println!("{}", "You must accept the terms to use the application.".red());
@@ -30,6 +36,12 @@ pub fn eula_checker() {
             exit(1);
         }
 
+    }
+    else if eula == true {
+        println!("Eula Accepted")
+    }
+    else {
+        println!("Your json file is corrupted, please replace it.")
     }
 
 }
